071124923e281b109a6043ec1ea089b0e1081e92
Georgi213 <77331773+Georgi213@users.noreply.github.com>
2021-12-08 15:51:13 +0200

    Add files via upload

---
+++ RockPaperScissorGame/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RockPaperScissorGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            // Clearing the images
+            computerpicturebox.Image = null;
+            PlayerPictureBox.Image = null;
+            panelPlayer.BackColor = Color.LightGray;
+            panelComputer.BackColor = Color.LightGray;
+            winnerLabel.Text = "................";
+            listBox1.Items.Clear();
+        }
+
+        private void rockPicturebox_Click(object sender, EventArgs e)
+        {
+            Random oRandomGenerator = new Random();
+            panelPlayer.BackColor = Color.Cornsilk;
+            panelComputer.BackColor = Color.Cornsilk;
+            int iComputerChoice;
+
+            PlayerPictureBox.Image = rockPicturebox.Image;
+
+            int iRandomNum = oRandomGenerator.Next(100, 4000);
+            iComputerChoice = (iRandomNum % 3) + 1;
+
+            switch (iComputerChoice)
+            {
+                case 1:
+
+                    computerpicturebox.Image = rockPicturebox.Image;
+                    winnerLabel.Text = "VIIK";
+                    listBox1.Items.Add("VIIK");
+                    break;
+
+                case 2:
+
+                    computerpicturebox.Image = paperPicturebox.Image;
+                    winnerLabel.Text = " Arvuti võidab, sest kivi lõhub käärid ";
+                    panelComputer.BackColor = Color.Crimson;
+                    listBox1.Items.Add("Võitis arvuti");
+                    break;
+
+                case 3:
+
+                    computerpicturebox.Image = scissorsPictureBox.Image;
+                    winnerLabel.Text = "  ";
+                    panelPlayer.BackColor = Color.Crimson;
+                    listBox1.Items.Add("Võitis arvuti");
+                    break;
+            }
+        }
+
+        private void paperPicturebox_Click(object sender, EventArgs e)
+        {
+            Random oRandomGenerator = new Random();
+            panelPlayer.BackColor = Color.Cornsilk;
+            panelComputer.BackColor = Color.Cornsilk;
+            int iComputerChoice;
+
+            PlayerPictureBox.Image = paperPicturebox.Image;
+
+            int iRandomNum = oRandomGenerator.Next(100, 4000);
+            iComputerChoice = (iRandomNum % 3) + 1;
+
+            switch (iComputerChoice)
+            {
+
+                case 1:
+
+                    computerpicturebox.Image = rockPicturebox.Image;
+                    winnerLabel.Text = " Sina võidad, sest käärid lõikavad paberit ";
+                    panelPlayer.BackColor = Color.Crimson;
+                    listBox1.Items.Add("Sina võidab");
+                    break;
+
+                case 2:
+
+                    computerpicturebox.Image = paperPicturebox.Image;
+                    winnerLabel.Text = "VIIK";
+                    listBox1.Items.Add("VIIK");
+                    break;
+
+                case 3:
+
+                    computerpicturebox.Image = scissorsPictureBox.Image;
+                    winnerLabel.Text = " Arvuti võidab, sest kivi lõhub käärid ";
+                    panelComputer.BackColor = Color.Crimson;
+                    listBox1.Items.Add("Võitis arvuti");
+                    break;
+
+            }
+
+        }
+
+        private void scissorsPictureBox_Click(object sender, EventArgs e)
+        {
+            Random oRandomGenerator = new Random();
+            panelPlayer.BackColor = Color.Cornsilk;
+            panelComputer.BackColor = Color.Cornsilk;
+            int iComputerChoice;
+
+            PlayerPictureBox.Image = scissorsPictureBox.Image;
+
+            int iRandomNum = oRandomGenerator.Next(100, 4000);
+            iComputerChoice = (iRandomNum % 3) + 1;
+
+            switch (iComputerChoice)
+            {
+
+                case 1:
+
+                    computerpicturebox.Image = rockPicturebox.Image;
+                    winnerLabel.Text = " Arvuti võidab, sest kivi lõhub käärid ";
+                    panelComputer.BackColor = Color.Crimson;
+                    listBox1.Items.Add("Võitis arvuti");
+                    break;
+
+                case 2:
+
+                    computerpicturebox.Image = paperPicturebox.Image;
+                    winnerLabel.Text = " Sina võidad, sest käärid lõikavad paberit ";
+                    panelPlayer.BackColor = Color.Crimson;
+                    listBox1.Items.Add("Sina võidad");
+                    break;
+
+                case 3:
+
+                    computerpicturebox.Image = scissorsPictureBox.Image;
+                    winnerLabel.Text = "VIIK";
+                    listBox1.Items.Add("VIIK");
+                    break;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            helpScreen1 help = new helpScreen1();
+            help.Show();
+        }
+    }
+}
---
+++ RockPaperScissorGame/helpScreen1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace RockPaperScissorGame
+{
+    partial class helpScreen1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(helpScreen1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(247, 114);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(293, 178);
+            this.textBox1.TabIndex = 20;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // helpScreen1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Name = "helpScreen1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
